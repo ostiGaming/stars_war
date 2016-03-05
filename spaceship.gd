79794fc8bf600877bd480cc0a42638b636eceae8
ZoidQC <zoid@leetzone.ca>
2016-03-05 18:15:28 -0500

    added line

--- spaceship.gd
+++ spaceship.gd
@@ -1,4 +1,4 @@
-extends Node
+extends RigidBody2D
 
 var LINEAR_FORCE = 150
 var ANGULAR_FORCE = 5
@@ -17,8 +17,15 @@ var right_pressed = false
 var spring
 
 func _draw():
-	draw_circle(get_pos(), 4, Color("#00FF00"))
-	update()
+	spring.set_node_b(get_path());
+	
+	var node_a = spring.get_node_a()
+	var node_b = spring.get_node_b()
+	
+	if (node_a != null && !node_a.is_empty() && node_b != null && !node_b.is_empty()):
+		var position = get_transform().basis_xform_inv(get_node(node_a).get_global_pos() - get_global_pos()) 
+		draw_line(position, Vector2(0, 0), Color("#000000"), 5)
+	
 
 func _ready():
 	spring = get_node("spring")
@@ -45,6 +52,8 @@ func _fixed_process(delta):
 	var local_force = get_transform().basis_xform(Vector2(0, forward_force))
 	set_applied_force(local_force)
 	set_angular_velocity(angular_velocity)
+	
+	update()
 
 func _input(event):
 	if (event.is_action_pressed(player_actions[player_index]["up"])):
