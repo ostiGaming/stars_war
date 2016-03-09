f686aa182ea151257a97ed950677e2eadf138742
micael.bergeron <micael.bergeron@solutionstlm.com>
2016-03-09 00:11:46 -0500

    added stars.

--- particles_attractors.gd
+++ particles_attractors.gd
@@ -1,10 +1,13 @@
 export var particle_group = "attracted"
+export var gravity = 0.5
+export var radius = 300
+export var absorption = 0
 
 func _ready():
 	for emitter in get_tree().get_nodes_in_group(particle_group):
 		var attractor = ParticleAttractor2D.new()
 		attractor.set_particles_path(emitter.get_path())
-		attractor.set_gravity(0.5)
-		attractor.set_radius(300)
-		attractor.set_absorption(0)
+		attractor.set_gravity(gravity)
+		attractor.set_radius(radius)
+		attractor.set_absorption(absorption)
 		add_child(attractor)
